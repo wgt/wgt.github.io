24f89da647db6d897aae5a229d0ee7cd057d43e3
yezhudaxue <dns@yezhudaxue.com>
2022-02-19 00:13:47 +0800

    add

--- cs2.sh
+++ cs2.sh
@@ -1,8 +1,9 @@
  #! /bin/bash
 
+cd ~
 yum install -y git vim g++ tmux
 git clone https://gitee.com/cpp-njuer-org/codeserver
-cp codeserver/code-server-4.0.2-linux-amd64.tar.gz  ./
-tar xvzf  code-server-4.0.2-linux-amd64.tar.gz
-cd code-server-4.0.2-linux-amd64
-export PASSWORD=123456 && ./code-server --host 0.0.0.0 --port 8080
+#cp codeserver/code-server-4.0.2-linux-amd64.tar.gz  ./
+cat codeserver/code-server-4.0.2-linux-amd64.tar.gz.0* | tar xvzf -
+#cd code-server-4.0.2-linux-amd64
+export PASSWORD=123456 && ./code-server-4.0.2-linux-amd64/code-server --host 0.0.0.0 --port 8080
